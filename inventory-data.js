c94c496ba0eee24aee273b5b61c258f6c4433ad7
marybethgabasjacinto-hash <marybethgabasjacinto@gmail.com>
2026-08-08 20:28:03 +0800

    Create inventory-data.js

---
+++ inventory-data.js
@@ -0,0 +1,53 @@
+const INVENTORY_DATA = {
+
+    middleHousing: [
+        {
+            project: "AGAPEYA EAST",
+            lotAlias: "AGM-02-052-001",
+            block: "Block 52",
+            lot: "Lot 01",
+            lotArea: 100,
+            floorArea: 50,
+            houseModel: "Duplex - PREMIUM",
+            grossContractPrice: 3490000
+        },
+
+        {
+            project: "AGAPEYA EAST",
+            lotAlias: "AGM-02-052-002",
+            block: "Block 52",
+            lot: "Lot 02",
+            lotArea: 110,
+            floorArea: 50,
+            houseModel: "Duplex - PREMIUM",
+            grossContractPrice: 3820000
+        }
+
+        // ...actual OPEN inventory continues
+    ],
+
+    middleCondo: [
+        {
+            project: "Terraces at Tradizo Enclave",
+            lotAlias: "TER-TE-A01-001",
+            buildingFloor: "A-01 / GF",
+            unitCode: "1",
+            floorArea: 12.5,
+            unitModel: "Parking",
+            grossContractPrice: 1010000
+        },
+
+        {
+            project: "Terraces at Tradizo Enclave",
+            lotAlias: "TER-TE-A01-004",
+            buildingFloor: "A-01 / GF",
+            unitCode: "4",
+            floorArea: 12.5,
+            unitModel: "Parking",
+            grossContractPrice: 1010000
+        }
+
+        // ...actual OPEN condo inventory continues
+    ]
+
+};
