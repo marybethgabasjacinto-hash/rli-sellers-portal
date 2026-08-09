176cf297d2a64c655e1fe288ea7f82d70280cceb
marybethgabasjacinto-hash <marybethgabasjacinto@gmail.com>
2026-08-09 10:35:04 +0800

    Update inventory-data.js

--- inventory-data.js
+++ inventory-data.js
@@ -1,73 +1,36 @@
-const INVENTORY_DATA = {
+middleHousing: [
 
-    // =========================================
-    // MIDDLE HOUSING
-    // =========================================
+    {
+        project: "AGAPEYA EAST",
+        lotAlias: "AGM-02-052-001",
+        block: "Block 52",
+        lot: "Lot 01",
+        lotArea: 100,
+        floorArea: 50,
+        houseModel: "Duplex - PREMIUM",
+        grossContractPrice: 3490000
+    },
 
-    middleHousing: [
+    {
+        project: "AGAPEYA EAST",
+        lotAlias: "AGM-02-053-001",
+        block: "Block 53",
+        lot: "Lot 01",
+        lotArea: 100,
+        floorArea: 50,
+        houseModel: "Duplex - PREMIUM",
+        grossContractPrice: 3490000
+    },
 
-        {
-            project: "AGAPEYA EAST",
-            lotAlias: "AGM-02-052-001",
-            block: "Block 52",
-            lot: "Lot 01",
-            lotArea: 100,
-            floorArea: 50,
-            houseModel: "Duplex - PREMIUM",
-            grossContractPrice: 3490000
-        },
+    {
+        project: "AGAPEYA EAST",
+        lotAlias: "AGM-02-054-001",
+        block: "Block 54",
+        lot: "Lot 01",
+        lotArea: 100,
+        floorArea: 50,
+        houseModel: "Duplex - PREMIUM",
+        grossContractPrice: 3490000
+    }
 
-        {
-            project: "AGAPEYA EAST",
-            lotAlias: "AGM-02-052-002",
-            block: "Block 52",
-            lot: "Lot 02",
-            lotArea: 110,
-            floorArea: 50,
-            houseModel: "Duplex - PREMIUM",
-            grossContractPrice: 3820000
-        },
-
-        {
-            project: "AGAPEYA EAST",
-            lotAlias: "AGM-02-052-003",
-            block: "Block 52",
-            lot: "Lot 03",
-            lotArea: 100,
-            floorArea: 50,
-            houseModel: "Duplex - PREMIUM",
-            grossContractPrice: 3490000
-        }
-
-    ],
-
-
-    // =========================================
-    // MIDDLE CONDO
-    // =========================================
-
-    middleCondo: [
-
-        {
-            project: "Terraces at Tradizo Enclave",
-            lotAlias: "TER-TE-A01-001",
-            buildingFloor: "A-01 / GF",
-            unitCode: "1",
-            floorArea: 12.5,
-            unitModel: "Parking",
-            grossContractPrice: 1010000
-        },
-
-        {
-            project: "Terraces at Tradizo Enclave",
-            lotAlias: "TER-TE-A01-004",
-            buildingFloor: "A-01 / GF",
-            unitCode: "4",
-            floorArea: 12.5,
-            unitModel: "Parking",
-            grossContractPrice: 1010000
-        }
-
-    ]
-
-};
+]
